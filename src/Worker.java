f51a795e624fd097af094846be59470fc484c063
ggnsta <39225304+ggnsta@users.noreply.github.com>
2018-05-29 16:13:17 +0300

    test

--- src/Worker.java
+++ src/Worker.java
@@ -11,8 +11,7 @@ public class Worker extends Thread implements Runnable {
     protected FileInputStream fis;
     protected FileOutputStream fos;
     protected ArrayList<String> fileList = new ArrayList<String>();
-    protected DataInputStream in;
-    protected DataOutputStream out;
+
     protected File history;
     protected File bufFile;
     protected ObjectOutputStream oos = null;
@@ -26,7 +25,7 @@ public class Worker extends Thread implements Runnable {
     public Worker(Socket clientSocket, MyGUI gui, Utility.TypeConection type) {
         this.clientSocket = clientSocket;
         this.gui = gui;
-        this.type=type;
+        this.type = type;
 
     }
 
@@ -47,33 +46,23 @@ public class Worker extends Thread implements Runnable {
             bufFile.getParentFile().mkdir();
             bufFile.createNewFile();
 
-            if(type==Utility.TypeConection.Server)
-            {
-                System.out.println("serv");
+            if (type == Utility.TypeConection.Server) {
+
                 oos = new ObjectOutputStream(clientSocket.getOutputStream());
-                System.out.println("serv2");
                 oos.flush();
                 ois = new ObjectInputStream(clientSocket.getInputStream());
-            }
-            else
-            {
+
+            } else {
                 System.out.println("client");
                 ois = new ObjectInputStream(clientSocket.getInputStream());
                 oos = new ObjectOutputStream(clientSocket.getOutputStream());
             }
 
-            out.flush();
-
-
-
-
-
-
             String str = this.getName();
             System.out.println("Есть контакт : " + str);
             while (true) {
 
-                //this.get();// собственно эти потоки создаются только для того, чтобы постоянно получать сообщения
+                this.get();// собственно эти потоки создаются только для того, чтобы постоянно получать сообщения
 
             }
 
@@ -85,23 +74,11 @@ public class Worker extends Thread implements Runnable {
     public void get() {
 
         try {
-            if (bufFile.length() == 0) return;//objectinputstream выкдиывает эксепшены, если входящий файл пустой
-            else {
 
-                ObjectInputStream oin = new ObjectInputStream(fis);
-                MessageObject buf = (MessageObject) oin.readObject();
+            MessageObject buf = (MessageObject) ois.readObject();
+            System.out.println(buf.senderName + ":" + buf.message);
+            gui.jtaTextAreaMessage.append(buf.message + "\n");
 
-                System.out.println(buf.senderName + ":" + buf.message);
-                gui.jtaTextAreaMessage.append(buf.message + "\n");
-
-                FileWriter fstream1 = new FileWriter(bufFile);
-                BufferedWriter out1 = new BufferedWriter(fstream1);
-                out1.write("");
-                out1.close();
-                fstream1.close();
-                oin.close();
-                System.out.print(bufFile.length());
-            }
         } catch (Exception x) {
             x.printStackTrace();
         }
@@ -109,16 +86,12 @@ public class Worker extends Thread implements Runnable {
 
     public void send() {
         try {
-            //  ObjectOutputStream oos = new ObjectOutputStream(clientSocket.getOutputStream());
-            //ObjectOutputStream oos = new ObjectOutputStream(fos);
+
             MessageObject mesObject = new MessageObject();
             mesObject.message = gui.jtfMessage.getText();// заносим сообщения из gui в объект
-
             System.out.println("send class worker");
-
-            //oos.writeObject(mesObject);
-            //oos.flush();
-            // oos.close();
+            oos.writeObject(mesObject);
+            oos.flush();
             gui.jtaTextAreaMessage.append(mesObject.message + "\n");//отображаем его в своём поле чата
 
 
